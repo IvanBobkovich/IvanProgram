6c03b85a5ff37d6e48887f3a9c69d3f744d0a78a
IvanBobkovich <85929898+IvanBobkovich@users.noreply.github.com>
2021-12-23 19:23:10 +0300

    Step_9_1

--- dealership/dealership/settings.py
+++ dealership/dealership/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'dealership_app'
+    'dealership_app.apps.DealershipsAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
